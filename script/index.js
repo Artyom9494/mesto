5e0652be53d19e01da791551ade8988ab9c0d64f
Artyom Kalinkin <artyom.artyom.9494@mail.ru>
2022-04-24 17:12:52 +0300

    ver 1.1

--- script/index.js
+++ script/index.js
@@ -1,47 +1,56 @@
 let popup = document.querySelector('.popup');
-let editButton = document.querySelector('.profile__Edit-Button');
+let editButton = document.querySelector('.profile__edit-button');
 let userName = document.querySelector('.profile__name');
 let userInfo = document.querySelector('.profile__info');
-let inputUserName = document.querySelector('.popup__name');
-let inputUserInfo = document.querySelector('.popup__achievements');
+let inputUserName = document.querySelector('.mod-name_mod-value');
+let inputUserInfo = document.querySelector('.mod-name_mod-value');
 let buttonSave = document.querySelector('.popup__button');
 let buttonClose = document.querySelector('.popup__close');
 let like = document.querySelector('.elements__like');
+let formElement = document.querySelector('.popup__form')
 
-inputUserName.value = userName.innerHTML;
-inputUserInfo.value = userInfo.innerHTML;
+inputUserName.value = userName.textContent;
+inputUserInfo.value = userInfo.textContent;
 
-function clos () {
+function closePopup () {
     popup.classList.toggle('popup_opened');
 };
-function inputValue () {
-    userName.innerHTML = inputUserName.value;
-    userInfo.innerHTML = inputUserInfo.value;
-};
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    userName.textContent = inputUserName.value;
+    userInfo.textContent = inputUserInfo.value;
+}
 
 editButton.addEventListener('click', function () {
+    userName.textContent = inputUserName.value;
+    userInfo.textContent = inputUserInfo.value;
     popup.classList.toggle('popup_opened');
 });
 
-buttonSave.addEventListener('click', function () {
-    inputValue()
-    clos();
+buttonSave.addEventListener('click', function (e) {
+    e.preventDefault()
+    userName.textContent = inputUserName.value;
+    userInfo.textContent = inputUserInfo.value;
+    closePopup();
 })
 
-buttonClose.addEventListener('click', function () { 
-    clos();
-});
+buttonClose.addEventListener('click', closePopup);
 
 popup.addEventListener('click', function (event) {
     if(event.target == event.currentTarget) {
-        clos();
+        closePopup();
     }
 });
 
 document.addEventListener('keyup', function (event) {
+    event.preventDefault()
     if( event.key == 'Enter') {
-        inputValue()
         popup.classList.remove('popup_opened');
+        formSubmitHandler();
     }
 })
 
+
+
+formElement.addEventListener('submit', formSubmitHandler);
