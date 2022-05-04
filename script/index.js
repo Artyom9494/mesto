b09a528408ef9ae34ee1aa0c693afcdc7e809fea
Artyom Kalinkin <artyom.artyom.9494@mail.ru>
2022-05-04 08:49:36 +0300

    create add elements

--- script/index.js
+++ script/index.js
@@ -1,3 +1,4 @@
+// первый попап доступы
 let popup = document.querySelector('.popup');
 let editButton = document.querySelector('.profile__edit-button');
 let userName = document.querySelector('.profile__name');
@@ -7,7 +8,7 @@ let inputUserInfo = document.querySelector('.popup__input_value_info');
 let buttonClose = document.querySelector('.popup__close');
 let formElement = document.querySelector('.popup__form')
 
-
+// первый попап функционал
 editButton.addEventListener('click', function () {
     inputUserName.value = userName.textContent;
     inputUserInfo.value = userInfo.textContent;
@@ -36,6 +37,14 @@ popup.addEventListener('click', function (event) {
     }
 });
 
+// второй попап доступы
+let popupAdd = document.querySelector('.popup-add');
+let editElements = document.querySelector('.profile__addbutton');
+let inputNamePlase = document.querySelector('.popup-add__input_value_name-plase');
+let inputUrl = document.querySelector('.popup-add__input_value_url');
+let closePopupAdd = document.querySelector('.popup-add__close');
+let addBut = document.querySelector('.popup-add__button');
+// let elementsTemplate = document.querySelector('#elements').content;
 
 
 const initialCards = [
@@ -64,6 +73,49 @@ const initialCards = [
       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
   ];
+
+// второй попап функционал
+editElements.addEventListener('click', () => {
+    popupAdd.classList.add('popup-add_opened');
+})
+
+function closeAdd () {
+    popupAdd.classList.toggle('popup-add_opened');
+}
+
+closePopupAdd.addEventListener('click', closeAdd);
+
+//// add new elements
+
+function addElements () {
+    let elementsTemplate = document.querySelector('#elements').content;
+    let elements = elementsTemplate.querySelector('.elements').cloneNode(true);
+    
+    elementsContainer.append(elements);
+}
+
+addBut.addEventListener('click', function () {
+    addElements();
+    closeAdd();
+}) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // document.addEventListener('keyup', function (event) {
 //     event.preventDefault()
 //     if( event.key == 'Enter') {
